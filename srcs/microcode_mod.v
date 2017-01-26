c3b07fc7ae1ca7067683382c8dcb65da08c5c168
Gerik Kubiak <gerikkub@gmail.com>
2017-01-25 21:42:56 -0800

    ld A, (rr) working

--- srcs/microcode_mod.v
+++ srcs/microcode_mod.v
@@ -5,17 +5,17 @@ module microcode_mod(
     output [58:0]control_signals 
     ); 
  
-    parameter opcode_table_size = 67; 
+    parameter opcode_table_size = 85; 
  
-    parameter subop_table_size = 31; 
+    parameter subop_table_size = 43; 
  
-    reg [4:0]opcode_table[0:opcode_table_size-1]; 
+    reg [5:0]opcode_table[0:opcode_table_size-1]; 
     reg [58:0]subop_table[0:subop_table_size-1]; 
  
     initial $readmemh("srcs/opcode_vector.txt", opcode_table); 
     initial $readmemh("srcs/subop_vector.txt", subop_table); 
      
-    wire [4:0]opcode_table_out; 
+    wire [5:0]opcode_table_out; 
     wire [58:0]subop_table_out; 
  
     assign opcode_table_out = opcode_table[opcode]; 
